bdc7c9658f975a8f534fb9b4bc655df431d72849
Jimmy <jimmychu82@gmail.com>
2020-10-02 16:01:37 -0700

    update on style guide

--- styleguide.php
+++ styleguide.php
@@ -1,3 +1,32 @@
-<div class="top-nav">
-  
-</div>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <title>Style guide</title>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body>
+
+    <div class="top-nav">
+
+    </div>
+
+
+    <!-- footer starts here -->
+
+    <footer>
+
+
+    </footer>
+
+    <!-- end of footer section -->
+
+  </body>
+</html>
