b3b4cc170ce4a4aa33779ac35044fc9ca3c172da
Jimmy <jimmychu82@gmail.com>
2020-10-02 23:15:27 -0700

    unsuccessful logo svg test

--- top-nav.php
+++ top-nav.php
@@ -7,14 +7,26 @@
 
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="css/fonts.css">
   </head>
 
   <body>
+    <div style="width: 100vw;">
 
-    <div class="top-nav">
-      
+      <div class="top-nav">
+        <div class="top-nav-item logo">
+          <img src="img/Logo.svg" alt="Logo image">
+        </div>
+
+        <div class="top-nav-item ">
+          <img src="img/Logo.svg" alt="Logo image">
+        </div>
+      </div>
+
+
     </div>
+
+
 
 
     <!-- footer starts here -->
