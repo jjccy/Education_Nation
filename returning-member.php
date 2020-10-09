fcd1a9830f4dd568aca0068a92e0c2f7a016f6cb
Terence Liu <liu_terence@hotmail.com>
2020-10-08 21:58:19 -0700

    Merge Conflict

--- returning-member.php
+++ returning-member.php
@@ -14,27 +14,78 @@
     $devider = strtok($devider, "\n"); // remove new line from fgets
     fclose($deviderFile);
     $usersList = explode($devider, $usersList);
+    $name = "";
     $email = "";
     $password= "";
+    $length = count($usersList);
 
-    foreach($usersList as $user){
+//     foreach($usersList as &$user){
+//       echo $user;
+//       echo "<br>";
+// }
+
+    // for ($i=0; $i<$length-3;$i=$i+4){
+    foreach($usersList as &$user){
      $loginInfo = $user;
-        $email = $loginInfo[0];
-        $password = $loginInfo[1];
+     echo $loginInfo;
+        // $email = $loginInfo[$i];
+        // $password = $loginInfo[$i+1];
+        // $name = $loginInfo[$i+2];
 
-    if($email == $_POST['email-input'] && $password == $_POST['password-input']){
-        //Login
-        $login = true;
-      }
+        // if($email==$_POST['email-input']){
+        //   if($password == $_POST['password-input']){
+        //       //Login
+        //       $login = true;
+        //       // alert box welcome
+        //       echo '<script language="javascript">';
+        //       $welcome = "alert('Welcome, " . $name . "');";
+        //       echo $welcome;
+        //       echo "window.location.href='index.php';";
+        //       echo '</script>';
+        //     }
+        //     elseif ($password != $_POST['password-input']) {
+        //       // alert box reject
+        //       echo '<script language="javascript">';
+        //       echo 'alert("Incorrect Password and/or Email");';
+        //       echo "window.location.href='login.php';";
+        //       echo '</script>';
+        //     }
+        //   }
+        //   else{
+        //     // alert box reject
+        //     echo '<script language="javascript">';
+        //     echo 'alert("Account Not Found");';
+        //     echo "window.location.href='login.php';";
+        //     echo '</script>';
+        //   }
     }
-    if ($email == $_POST['email-input'] && $password != $_POST['password-input']) {
-      echo '<script>alert("Account Not Found");</script>';
-    }
-    else{
-     echo '<script>alert("Incorrect Email or Password.");</script>';
 
-    }
 
-     ?>
+
+
+      ?>
+      <?php
+    //     $handle = fopen("theo-testing.txt", "r") or die("Unable to open file!");
+    //     $devider_file = fopen("devider.txt", "r") or die("Unable to open file!");
+    //     $currentUser = "Terence";
+    //
+    //     while (!feof($devider_file)) {
+    //         $d = fgets($devider_file);
+    //     }
+    //
+    //     while (!feof($handle)) {
+    //         // echo fgets($handle) . "<br>";
+    //         $currentLine = fgets($handle);
+    //         // echo $currentLine . "<br>";
+    //
+    //         // echo $d;
+    //
+    //         $user_data = explode(strval($d), strval($currentLine));
+    //         echo $user_data[0] . "<br>";
+    //     }
+    //
+    //     fclose($handle);
+    //     fclose($devider_file);
+    // ?>
   </body>
 </html>
