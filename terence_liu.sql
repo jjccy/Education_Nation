dea74d7f3b495ad98cf43a0071625760de64ad72
Theo <tatang@sfu.ca>
2020-10-30 15:03:42 -0700

    Uploading DB

---
+++ terence_liu.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2020 at 10:31 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `terence_liu`
+--
+CREATE DATABASE IF NOT EXISTS `terence_liu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `terence_liu`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hasbooking`
+--
+
+DROP TABLE IF EXISTS `hasbooking`;
+CREATE TABLE IF NOT EXISTS `hasbooking` (
+  `b_id` INT NOT NULL AUTO_INCREMENT,
+  `tutor_id` INT NOT NULL,
+  `s_id` INT NOT NULL,
+  `time_start` datetime NOT NULL,
+  `time_end` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `price` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`b_id`,`tutor_id`,`s_id`),
+  KEY `tutor_id` (`tutor_id`),
+  KEY `s_id` (`s_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE IF NOT EXISTS `member` (
+  `m_id` INT NOT NULL AUTO_INCREMENT,
+  `name` char(30) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` char(128) DEFAULT NULL,
+  `profile_address` varchar(260) DEFAULT NULL,
+  PRIMARY KEY (`m_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE IF NOT EXISTS `review` (
+  `r_id` INT NOT NULL AUTO_INCREMENT,
+  `student_id` INT NOT NULL,
+  `tutor_id` INT NOT NULL,
+  `data_posted` timestamp NOT NULL DEFAULT current_timestamp(),
+  `rating` decimal(1,0) DEFAULT NULL,
+  `comments` mediumtext DEFAULT NULL,
+  PRIMARY KEY (`r_id`,`student_id`,`tutor_id`),
+  KEY `student_id` (`student_id`),
+  KEY `tutor_id` (`tutor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signsup`
+--
+
+DROP TABLE IF EXISTS `signsup`;
+CREATE TABLE IF NOT EXISTS `signsup` (
+  `b_id` INT NOT NULL,
+  `student_id` INT NOT NULL,
+  PRIMARY KEY (`b_id`,`student_id`),
+  KEY `student_id` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `student_id` INT NOT NULL,
+  `cur_grade` decimal(2,0) DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+CREATE TABLE IF NOT EXISTS `subject` (
+  `s_id` INT NOT NULL AUTO_INCREMENT,
+  `min_grade` decimal(2,0) DEFAULT NULL,
+  `max_grade` decimal(2,0) DEFAULT NULL,
+  `subject_name` char(30) DEFAULT NULL,
+  PRIMARY KEY (`s_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teaches`
+--
+
+DROP TABLE IF EXISTS `teaches`;
+CREATE TABLE IF NOT EXISTS `teaches` (
+  `m_id` INT NOT NULL,
+  `s_id` INT NOT NULL,
+  PRIMARY KEY (`s_id`,`m_id`),
+  KEY `m_id` (`m_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor`
+--
+
+DROP TABLE IF EXISTS `tutor`;
+CREATE TABLE IF NOT EXISTS `tutor` (
+  `tutor_id` INT NOT NULL,
+  `balance` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`tutor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `hasbooking`
+--
+ALTER TABLE `hasbooking`
+  ADD CONSTRAINT `hasbooking_ibfk_1` FOREIGN KEY (`tutor_id`) REFERENCES `tutor` (`tutor_id`),
+  ADD CONSTRAINT `hasbooking_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `subject` (`s_id`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`tutor_id`) REFERENCES `tutor` (`tutor_id`);
+
+--
+-- Constraints for table `signsup`
+--
+ALTER TABLE `signsup`
+  ADD CONSTRAINT `signsup_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `hasbooking` (`b_id`),
+  ADD CONSTRAINT `signsup_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `member` (`m_id`);
+
+--
+-- Constraints for table `teaches`
+--
+ALTER TABLE `teaches`
+  ADD CONSTRAINT `teaches_ibfk_1` FOREIGN KEY (`m_id`) REFERENCES `tutor` (`tutor_id`),
+  ADD CONSTRAINT `teaches_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `subject` (`s_id`);
+
+--
+-- Constraints for table `tutor`
+--
+ALTER TABLE `tutor`
+  ADD CONSTRAINT `tutor_ibfk_1` FOREIGN KEY (`tutor_id`) REFERENCES `member` (`m_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
