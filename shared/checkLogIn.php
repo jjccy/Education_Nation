8c44d55e2274a436ff727d9a1b50fcd56fabb873
Theo <tatang@sfu.ca>
2020-11-08 18:57:45 -0800

    Restricting students from accessing tutor pages

--- shared/checkLogIn.php
+++ shared/checkLogIn.php
@@ -4,4 +4,14 @@
         echo "window.location.href='login.php';";
         echo '</script>';
     }
+
+    if (!isset($_COOKIE['isTutor'])) {
+        echo "<script>";
+            echo "var current_url = window.location.pathname;";
+            echo 'if (!(current_url.includes("account-settings") || current_url.includes("upcoming-booking") || current_url.includes("booking-history"))) {';
+                echo "window.location.href='index.php';";
+                echo "alert('Restricted, you do not have access!');";
+            echo '}';
+        echo "</script>";
+    }
 ?>
